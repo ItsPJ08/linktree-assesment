ca675c8d5a2ce5a203273a50351155b936dcf41f
ItsPj08 <iampj2002@gmail.com>
2026-06-06 17:41:11 -0700

    Linktree project assesment

---
+++ README.md
@@ -0,0 +1,20 @@
+# Personal Linktree
+
+Built a modern Linktree page with React, Vite and CSS. Links include Instagram, LinkedIn, GitHub, and my personal Resume. 
+Includes Profile picture and a small bio, as well as, Javascript features to make the page feel more interactive
+
+
+## Features
+
+- Profile Section
+- Links to LinkedIn, GitHub, Resume, and Instagram
+- Custom CSS layout
+- Javascript feature: time-based greeting and day/night mode + toggle button 
+## How to run it 
+
+Simply click the link:
+
+GitHub Repo: 
+
+## Javascript feature
+I created a time based greeting and day/night mode toggle as my feature. I chose this because I initially realized my color palette was a bit too bright when viewing in darker areas/times. So to help ease the eyes, I created a Night mode feature which should automatically turn on when the time hits 6PM to 6AM. I also created a greeting along with this to make it feel more interactive. 
---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+import profileImg from './assets/profile.jpeg';
+import InstaImg from './assets/Instagramlogo.png';
+import GithubImg from './assets/githublogo.png';
+import LinkedinImg from './assets/linkedinlogo.png';
+import ResumeImg from './assets/resumelogo.png';
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const greeting = new Date().getHours();
+  let message;
+ 
+  if (greeting >= 5 && greeting < 12)
+  {
+    message = "Good Morning!";
+  }
+    else if (greeting >= 12 && greeting < 18)
+    {
+      message = "Good Afternoon!";
+    }
+    else
+    {
+      message = "Good Evening!";
+    }
+  
+useEffect(() => {
+    const dayNight = new Date().getHours();
+
+    if (dayNight >= 18|| dayNight < 6) {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
+    }
+  }, []);
+
+
+  return (
+    <>
+    <main className={darkMode ? 'app dark' : 'app'}> {/* Apply 'dark' class when darkMode is true */}
+      <button className="dark-mode-btn" onClick={() => setDarkMode(!darkMode)}> {/* Toggles dark mode button on click */}
+        {darkMode ? 'Day Mode' : 'Night Mode'}
+      </button>
+      <section className="profile-card">
+        <img src={profileImg}  alt="Prabjot profile" className="profile-img" />
+       <h1>Prabjot Pannu</h1>
+       <p> Aspiring Software Engineer | Gilroy, CA | Bachelor of Science: Computer Science, CSUMB </p>
+      </section>
+
+ 
+      <section className="images-place">
+  <div className="icons">
+    <div className="icon-card">
+    <a href = "https://www.instagram.com/itspj__" target="_blank" rel="noopener noreferrer"><img src={InstaImg} alt="Instagram logo" className="link-icon" /> 
+    <p>Instagram</p> </a>
+    </div>
+    <div className="icon-card">
+     <a href = "https://www.github.com/ItsPJ08" target="_blank" rel="noopener noreferrer">
+      <img src={GithubImg} alt="GitHub logo" className="link-icon" />
+      <p>GitHub</p> </a>
+    </div>
+    <div className="icon-card">
+      <a href="https://www.linkedin.com/in/prabjot-pannu-b93955328/"  target="_blank" rel="noopener noreferrer">
+      <img src={LinkedinImg} alt="LinkedIn logo" className="link-icon" /> 
+      <p>LinkedIn</p> </a>
+    </div>
+  <div className="icon-card">
+      <a href = "/PrabjotResume.pdf"  target="_blank" rel="noopener noreferrer">
+      <img src={ResumeImg} alt="Resume" className="link-icon" /> 
+      <p>My Resume</p> </a>
+      
+    </div>
+  </div>
+
+</section>
+<section className = "greeting-place"> 
+  <h2>{message}</h2>
+
+
+</section>
+ 
+
+   
+     
+  
+
+
+    </main>
+      
+    </>
+  )
+}
+
+export default App
